ceb48a14094df326977f0c83f3e617051ba99a11
Jenson <cxchooi@gmail.com>
2025-04-27 13:20:11 +0800

    Updatedd app.py's coding to match with quiz_routes.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from quiz_routes import quiz
 
 app = Flask(__name__)
 
+# Register your blueprint
+app.register_blueprint(quiz)
+
 @app.route('/')
 def home():
-    return '<h1>Welcome to Kuizpedia!</h1><p>Flask is running 🎉</p>'
+    return '<h1>Welcome to Kuizpedia!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
