f21ea9ba610fffb368e6b490d93c72d33fe3f536
qsrhzrn <qaisarah.azran@gmail.com>
2025-05-30 20:29:36 +0800

    Update port

--- backend/app.py
+++ backend/app.py
@@ -68,5 +68,7 @@ def add_sample_data():
 with app.app_context():
     db.create_all()
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
